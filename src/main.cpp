6fe285428b070fadad9a5294d4abebef3b7eb8bc
asmerovski <asmerovski@gmail.com>
2026-06-26 10:38:10 +0200

    Changed organization name and version

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     app.setApplicationName("SonifiQ");
-    app.setApplicationVersion("1.0.2");
-    app.setOrganizationName("SonifiQ");
+    app.setApplicationVersion("1.2.1");
+    app.setOrganizationName("AppSMall");
     app.setWindowIcon(QIcon(":/icons/res/icons/app.svg"));
 
     MainWindow w;
